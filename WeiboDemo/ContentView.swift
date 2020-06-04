85d2f72deac26905a47d50ce49de77c4cc6e3473
dashu <bigtree2018102@gmail.com>
2020-06-04 14:58:22 +0700

    reset add

---
+++ WeiboDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeiboDemo
+//
+//  Created by SHU on 6/3/20.
+//  Copyright © 2020 SHU. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World！")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
